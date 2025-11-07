e93a1933f78930fc39883098f052b270350fffbe
matheus <matheusbispo0404@gmail.com>
2025-11-06 22:16:17 -0300

    test: realocando os valores de km para incremental

--- data_feeder.py
+++ data_feeder.py
@@ -1,15 +1,15 @@
-import requests
-import random
-import time
+import requests, random, time
 
 IOT_AGENT_URL = "http://20.63.91.180:7896/iot/d?k=health-api-key&i=dev-esp32-001"
+distance_total = 0.0  # valor acumulado
 
 while True:
     body_temp = round(random.uniform(36.0, 38.5), 1)
     heart_rate = random.randint(60, 140)
-    distance_km = round(random.uniform(0.0, 0.5), 4)
+    distance_increase = round(random.uniform(0.01, 0.05), 4)  # simula progresso
+    distance_total += distance_increase
 
-    payload = f"t|{body_temp}|h|{heart_rate}|d|{distance_km}"
+    payload = f"t|{body_temp}|h|{heart_rate}|d|{distance_total}"
     headers = {"Content-Type": "text/plain"}
 
     try:
